dd03966e173a708055c2dd17281e93d9ed90a0ce
Perry Campbell <51770549+pc02130@users.noreply.github.com>
2021-05-19 10:21:51 -0400

    Update KeyboardToolBar.swift

--- KeyboardToolBar.swift
+++ KeyboardToolBar.swift
@@ -1,37 +1,48 @@
+//
+//  KeyboardToolBar.swift
+//  SpreadRatePro1.2
+//
+//  Created by Perry Campbell on 5/14/21.
+//
+
 import SwiftUI
 
 final class KeyboardResponder: ObservableObject {
-    
-    private var notificationCenter: NotificationCenter
+    private var notificationCeneter: NotificationCenter
+    private var enabled: Bool = false
     @Published private(set) var currentHeight: CGFloat = 0
-
+    
     init(center: NotificationCenter = .default) {
-        notificationCenter = center
-        notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCeneter = center
+        notificationCeneter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCeneter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
     func dismiss() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
     
     deinit {
-        notificationCenter.removeObserver(self)
+        notificationCeneter.removeObserver(self)
     }
-
-    @objc func keyBoardWillShow(notification: Notification) {
+    
+    @objc func keyBoardWillShow(notification: Notification ) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             currentHeight = keyboardSize.height
+            enabled = true
         }
     }
-
     @objc func keyBoardWillHide(notification: Notification) {
         currentHeight = 0
+        enabled = false
+    }
+    func isEnabled() -> Bool {
+        return enabled
     }
 }
 
-struct KeyboardView<Content, ToolBar> : View where Content : View, ToolBar: View {
-    @StateObject private var keyboard: KeyboardResponder = KeyboardResponder()
+struct KeyboardView<Content, ToolBar> : View where Content : View, ToolBar : View {
+    @StateObject private var keyboard : KeyboardResponder = KeyboardResponder()
     let toolbarFrame: CGSize = CGSize(width: UIScreen.main.bounds.width, height: 40.0)
     var content: () -> Content
     var toolBar: () -> ToolBar
@@ -41,16 +52,16 @@ struct KeyboardView<Content, ToolBar> : View where Content : View, ToolBar: View
             content()
                 .padding(.bottom, (keyboard.currentHeight == 0) ? 0 : toolbarFrame.height)
             VStack {
-                 Spacer()
-                 toolBar()
+                Spacer()
+                toolBar()
                     .frame(width: toolbarFrame.width, height: toolbarFrame.height)
-                    .background(Color.secondary)
+                    .background(Color("buttonShadow"))
+                    .opacity(0.7)
             }.opacity((keyboard.currentHeight == 0) ? 0 : 1)
             .animation(.easeOut)
         }
         .padding(.bottom, keyboard.currentHeight)
         .edgesIgnoringSafeArea(.bottom)
         .animation(.easeOut)
-        
     }
 }
